68a9366adf3d2c66a6626e056f2fff49a663b04b
Deuslleyw <Deuslleyw@hotmail.com>
2024-09-05 13:48:00 -0300

    init project

---
+++ src/main/java/com/deusley/api_listas/ApiListasApplication.java
@@ -0,0 +1,13 @@
+package com.deusley.api_listas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiListasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiListasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deusley/api_listas/ApiListasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusley.api_listas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiListasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
